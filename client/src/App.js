7cf55a00afae4d7ad58bbef5d29940e180c7bf56
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-04-14 13:42:54 +0530

    Add socket.io-client dependency and implement basic messaging in App component; remove unused files and CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Hello from client!" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Enter message" />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
